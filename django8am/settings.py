ea58d713ebcfc4c62f184b52d601700a4f71a18c
dpdahal <dp48dahal@gmail.com>
2023-04-21 09:42:29 +0545

    update

--- django8am/settings.py
+++ django8am/settings.py
@@ -131,8 +131,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'online2020programming@gmail.com'
-EMAIL_HOST_PASSWORD = 'tnfiyymzmvsmupkg'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 
